db13e577993f97defc99c2ca6b72680379117847
pavanvamsi1 <valivetiravisaipavanvamsi@gmail.com>
2026-02-16 19:09:23 +0530

    files added

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
